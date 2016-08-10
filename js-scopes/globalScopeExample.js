72b1ad8b33a7009c53e766c0683c61433aa4f0ed
echoavi <you@example.com>
2016-08-10 18:02:10 +0530

    on example added with global scope varibale

---
+++ js-scopes/globalScopeExample.js
@@ -0,0 +1,26 @@
+console.log('Hello World');//global namespace
+global.console.log('Hello World');//same as console.log
+
+globalVar = 'globalVar';//global scope
+var a = 17;//local scope
+console.log(a)//17
+console.log(this.a);// undefined as a is in local scope and this referes module scope.
+module.exports.a = a;// a is now in module scope
+console.log(this.a);// 17 as a is in module scope and this referes module scope.
+console.log(this.globalVar);// undefined as globalVar is in global scope and this referes module scope.
+
+function testFunction(){
+  console.log('in testFunction module scope variable a : '+this.a)//module scope variable
+  console.log('in testFunction global scope variable globalVar : '+this.globalVar);//global scope variable
+};
+console.log('========testFunction will be called in global scope===========');
+testFunction(); //calling this way function is called in global scope.
+
+console.log('========testFunction will be called in module scope===========');
+testFunction.call(this);
+
+module.exports.testFunction = testFunction;
+console.log('========testFunction will be called in module scope===========');
+this.testFunction(); //calling this way function is called in module scope.
+
+
